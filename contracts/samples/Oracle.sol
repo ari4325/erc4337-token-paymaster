fae6b9a00f5630be0cdbcd0441dfd3fe8f04742f
ari4325 <ari4325@live.com>
2023-05-31 12:15:56 +0530

    token paymaster updated with oracle

---
+++ contracts/samples/Oracle.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract Oracle {
+
+    address _base; 
+    address _quote;
+    uint8 _decimals;
+
+    constructor(
+        address base,
+        address quote,
+        uint8 decimals
+    ) {
+        _base = base;
+        _quote = quote;
+        _decimals = decimals;
+    }
+
+    function getLatestPrice() public view returns (int256) {
+        require(
+            _decimals > uint8(0) && _decimals <= uint8(18),
+            "Invalid _decimals"
+        );
+        int256 decimals = int256(10 ** uint256(_decimals));
+        (, int256 basePrice, , , ) = AggregatorV3Interface(_base)
+            .latestRoundData();
+        uint8 baseDecimals = AggregatorV3Interface(_base).decimals();
+        basePrice = scalePrice(basePrice, baseDecimals);
+
+        (, int256 quotePrice, , , ) = AggregatorV3Interface(_quote)
+            .latestRoundData();
+        uint8 quoteDecimals = AggregatorV3Interface(_quote).decimals();
+        quotePrice = scalePrice(quotePrice, quoteDecimals);
+
+        return (basePrice * decimals) / quotePrice;
+    }
+
+    function scalePrice(
+        int256 _price,
+        uint8 _priceDecimals
+    ) internal view returns (int256) {
+        if (_priceDecimals < _decimals) {
+            return _price * int256(10 ** uint256(_decimals - _priceDecimals));
+        } else if (_priceDecimals > _decimals) {
+            return _price / int256(10 ** uint256(_priceDecimals - _decimals));
+        }
+        return _price;
+    }
+}
